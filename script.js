aa359b5a742867f4d14b888eab236593eb690fcc
tkhoangpham1999-ship-it <tkhoangpham1999@gmail.com>
2026-05-01 19:31:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const noMessages = [
 ]
 
 const yesTeasePokes = [
-    "try saying no first... I bet you want to know what happens 😏",
+    "try saying no first... I bet you're curious what happens 😏",
     "go on, hit no... just once 👀",
     "you're missing out 😈",
     "click no, I dare you 😏"
